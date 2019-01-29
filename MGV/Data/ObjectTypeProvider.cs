b83a3f9e98992418e868e37adbff9743d5477f11
Julii Vedkal <julii.vedkal@ssa.group>
2019-01-29 15:07:34 +0200

    [added] - add ability interact with multiple files

---
+++ MGV/Data/ObjectTypeProvider.cs
@@ -0,0 +1,44 @@
+﻿using MGV.Models;
+using System;
+using System.Collections.Generic;
+
+namespace MGV.Data
+{
+    public static class ObjectTypeProvider
+    {
+        #region Private Fields
+
+        private static readonly Dictionary<Type, ObjectType> Types = new Dictionary<Type, ObjectType>
+        {
+            {typeof(Quiz), ObjectType.Quiz },
+            {typeof(Rule), ObjectType.Rule },
+            {typeof(Stage), ObjectType.Stage },
+            {typeof(Ending), ObjectType.Ending },
+            {typeof(Question), ObjectType.Question },
+        };
+
+        #endregion Private Fields
+
+        #region Public Enums
+
+        public enum ObjectType
+        {
+            Quiz = 10,
+            Rule = 20,
+            Stage = 30,
+            Ending = 40,
+            Question = 50
+        }
+
+        #endregion Public Enums
+
+        #region Public Methods
+
+        public static ObjectType For(Type type)
+        {
+            return Types[type];
+        }
+
+        #endregion Public Methods
+    }
+}
---
+++ MGV/Data/Repositories/FileObjectRepository.cs
@@ -0,0 +1,92 @@
+﻿using MGV.Models;
+using Microsoft.Data.Sqlite;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+
+namespace MGV.Data.Repositories
+{
+    public class FileObjectRepository
+    {
+        #region Private Fields
+
+        private readonly string _connectionString;
+        private readonly DatabaseInterface _databaseInterface;
+        private readonly ILogger _logger;
+
+        #endregion Private Fields
+
+        #region Public Constructors
+
+        public FileObjectRepository(string connectionString, ILogger logger)
+        {
+            _connectionString = connectionString;
+            _logger = logger;
+            _databaseInterface = DatabaseInterface.GetInstance(_logger);
+        }
+
+        #endregion Public Constructors
+
+        #region Public Methods
+
+        public void AddFileToObject(BaseEntity item, int fileId)
+        {
+            using (var connection = new SqliteConnection(_connectionString))
+            {
+                bool result = _databaseInterface.ExecuteCustomQuery(
+                        "INSERT INTO Files_Objects(FileId, ObjectId, ObjectType)" +
+                        "Values(@fileId, @objectId, @objectType)",
+                        connection,
+                        new SqliteParameter { ParameterName = "@fileId", DbType = DbType.Int32, Value = fileId },
+                        new SqliteParameter { ParameterName = "@objectId", DbType = DbType.Int32, Value = item.Id },
+                        new SqliteParameter { ParameterName = "@objectType", DbType = DbType.Int32, Value = ObjectTypeProvider.For(item.GetType()) }
+                    );
+                if (!result)
+                {
+                    _logger.LogError($"File not added: {item.Name}, {fileId}");
+                }
+            }
+        }
+
+        public void DeleteFileFromObject(BaseEntity item, int fileId)
+        {
+            using (var connection = new SqliteConnection(_connectionString))
+            {
+                bool result = _databaseInterface.ExecuteCustomQuery(
+                        "Delete From Files_Objects " +
+                        "Where FileID = @fileId and ObjectId = @objectId and ObjectType = @objectType",
+                        connection,
+                        new SqliteParameter { ParameterName = "@fileId", DbType = DbType.Int32, Value = fileId },
+                        new SqliteParameter { ParameterName = "@ObjectId", DbType = DbType.Int32, Value = item.Id },
+                        new SqliteParameter { ParameterName = "@fileId", DbType = DbType.Int32, Value = ObjectTypeProvider.For(item.GetType()) }
+                    );
+                if (!result)
+                {
+                    _logger.LogError($"FileObject not removed: {fileId}, {item.Id}, {item.GetType()}");
+                }
+            }
+        }
+
+        public IEnumerable<int> GetAllFileIdForObject(BaseEntity item)
+        {
+            IEnumerable<int> result = Enumerable.Empty<int>();
+
+            using (var connection = new SqliteConnection(_connectionString))
+            {
+                result = _databaseInterface.GetSimpleDataList<int>(
+                        "Select Files_Objects.FileId From Files_Objects" +
+                        "Where ObjectId = @id And ObjectType = @objectType",
+                        "FileId",
+                        connection,
+                        new SqliteParameter { ParameterName = "@objectId", DbType = DbType.Int32, Value = item.Id },
+                        new SqliteParameter { ParameterName = "@objectType", DbType = DbType.Int32, Value = ObjectTypeProvider.For(item.GetType()) }
+                    );
+            }
+
+            return result;
+        }
+
+        #endregion Public Methods
+    }
+}
