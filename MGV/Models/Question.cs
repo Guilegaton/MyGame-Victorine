d5d1c4debc0ac9fc22e398fc9dc1286974a18aa8
Julii Vedkal <julii.vedkal@ssa.group>
2019-02-11 13:02:12 +0200

    [updated] - add types for questions

--- MGV/Models/Question.cs
+++ MGV/Models/Question.cs
@@ -6,8 +6,21 @@
         #region Public Properties
 
         public int Cost { get; set; }
+        public QuestionTypes Type { get; set; }
         public int StageId { get; set; }
 
         #endregion Public Properties
+
+        #region Public Enums
+
+        public enum QuestionTypes : long
+        {
+            Music,
+            Image,
+            Text,
+            Video
+        }
+
+        #endregion Public Enums
     }
 }
